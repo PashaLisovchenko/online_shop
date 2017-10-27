40b53521999357ae01289b10397ce7581850bf30
pasha <lisovchenko.pasha@gmail.com>
2017-10-27 22:06:57 +0300

    ADD application 'shop'

---
+++ shop/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.ProductList.as_view(), name='product_list'),
+    url(r'^(?P<category_slug>[-\w]+)/$', views.ProductListByCategory.as_view(), name='product_list_by_category'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.ProductDetail.as_view(), name='product_detail'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.edit import FormMixin
+from .models import Category, Product
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class ProductList(ListView):
+    template_name = 'list.html'
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data(**kwargs)
+        category = None
+        categories = Category.objects.all()
+        products = Product.available_true.all()
+        context['category'] = category
+        context['categories'] = categories
+        context['products'] = products
+        return context
+
+
+class ProductListByCategory(DetailView):
+    template_name = 'list.html'
+    model = Category
+    context_object_name = 'category'
+    slug_url_kwarg = 'category_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListByCategory, self).get_context_data(**kwargs)
+        categories = Category.objects.all()
+        products = Product.available_true.filter(category=context['category'])
+        context['categories'] = categories
+        context['products'] = products
+        return context
+
+
+class ProductDetail(DetailView):
+    template_name = 'detail.html'
+    model = Product
+    context_object_name = 'product'
+    pk_url_kwarg = 'id'
+    slug_url_kwarg = 'slug'
